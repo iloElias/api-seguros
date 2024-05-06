12933a9364028d40e741ec40ae957dd82abcdec1
murilo.elias <muriloelias@ipeweb.com.br>
2024-05-05 22:11:34 -0300

    [update] - typescript update

--- src/model/creditScoreModel.js
+++
@@ -1,3 +0,0 @@
-exports.calculate = (data) => {
-    return Math.random();
-};
--- src/routes/creditScoreRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const creditScoreController = require('../controller/creditScoreController');
-
-router.post('/', creditScoreController.calculateCreditScore);
-
-module.exports = router;
